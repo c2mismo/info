b2e669ff05d8a26c391510a2e10cbf19933cc09f
c2mismo <c2mismo@gmail.com>
2020-03-31 02:02:17 +0200

    test prettifier VScode

--- Informatica/HTML/Plantillas/indexhtml/js/nav.js
+++ Informatica/HTML/Plantillas/indexhtml/js/nav.js
@@ -1,26 +1,26 @@
 // Tamaño < 600px oculto los botones
 // y añade un boton menu para mostrar los otros
-function navMenu(){
-	let variable = document.getElementsByClassName('navBar');
-	for(let i = 0; i < variable.length; i++){
-		variable[i].classList.toggle('hideLikeMovil');
-	}
+function navMenu() {
+  let variable = document.getElementsByClassName("navBar");
+  for (let i = 0; i < variable.length; i++) {
+    variable[i].classList.toggle("hideLikeMovil");
+  }
 }
 // muestran y ocultan los menus temáticos
-function navMenuArduino(){
-	document.getElementById('menuArduino').classList.toggle('hidden');
-	document.getElementById('menuElectronica').classList.add('hidden');
-	document.getElementById('menuInformatica').classList.add('hidden');
+function navMenuArduino() {
+  document.getElementById("menuArduino").classList.toggle("hidden");
+  document.getElementById("menuElectronica").classList.add("hidden");
+  document.getElementById("menuInformatica").classList.add("hidden");
 }
 
-function navMenuElectronica(){
-	document.getElementById('menuElectronica').classList.toggle('hidden');
-	document.getElementById('menuArduino').classList.add('hidden');
-	document.getElementById('menuInformatica').classList.add('hidden');
+function navMenuElectronica() {
+  document.getElementById("menuElectronica").classList.toggle("hidden");
+  document.getElementById("menuArduino").classList.add("hidden");
+  document.getElementById("menuInformatica").classList.add("hidden");
 }
 
-function navMenuInformatica(){
-	document.getElementById('menuInformatica').classList.toggle('hidden');
-	document.getElementById('menuArduino').classList.add('hidden');
-	document.getElementById('menuElectronica').classList.add('hidden');
+function navMenuInformatica() {
+  document.getElementById("menuInformatica").classList.toggle("hidden");
+  document.getElementById("menuArduino").classList.add("hidden");
+  document.getElementById("menuElectronica").classList.add("hidden");
 }
