7e6409780ba75174cc494e60ed390767442994f8
c2mismo <c2mismo@gmail.com>
2020-04-07 20:36:32 +0200

    End media print whith error into scroll.js
    Prints initBtn need test whith:
    onbeforeprint

--- informatica/html/plantillas/indexhtml/js/scroll.js
+++ informatica/html/plantillas/indexhtml/js/scroll.js
@@ -9,13 +9,17 @@ var c2m = document.getElementById("c2m");
 var openNavBtn = document.getElementById("openNavBtn");
 
 var mQ600 = window.matchMedia("(max-width: 600px)");
+var mPrint = window.matchMedia("print");
+
+
+
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 // When the user scrolls down 20px from the top of the document, show the button
-
 function scrollFunction() {
+
   if (mQ600.matches){
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       initBtn.style.display = "block";
@@ -33,10 +37,74 @@ function scrollFunction() {
       c2m.style.display = "none";
     } else {
       initBtn.style.display = "none";
-      c2m.style.display = "block";
+      c2m.style.display = "inline-block";
     }
   }
 }
+
+/* ERROR AL OCULTAR EL initBtn
+
+hemos probado:
+console.log(mPrint);
+
+y efectivamente muestra el media print
+probamos:
+
+/*
+function scrollFunction() {
+  if (mPrint.matches){
+    initBtn.style.display = "none";
+  } else {
+    if (mQ600.matches){
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        initBtn.style.display = "block";
+        c2m.style.display = "none";
+        openNavBtn.style.display = "none";
+      } else {
+        initBtn.style.display = "none";
+        c2m.style.display = "inline-block";
+        openNavBtn.style.display = "inline-block";
+      }
+
+    } else {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        initBtn.style.display = "block";
+        c2m.style.display = "none";
+      } else {
+        initBtn.style.display = "none";
+        c2m.style.display = "block";
+      }
+    }
+  }
+}
+
+y no lo oculta pienso que es porque la funcion se ejecuta al mover el ratón
+
+hemos probado en la Linea 14
+
+if (mPrint.matches){
+  initBtn.style.display = "none";
+}
+
+tampoco funcionó
+probamos en la misma linea
+
+
+mPrint.addListener(function(mql) {
+  if(mql.matches) {
+    console.log('webkit equivalent of onbeforeprint');
+  }
+});
+
+
+deberíamos probar con
+
+onbeforeprint :(
+
+
+*/
+
+
 
 
 
